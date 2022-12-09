1e56f8bb997953b34e352d022865b6cdd8b9a61f
HuettenaTHOR <frederik.huettemann@gmail.com>
2022-12-09 12:50:51 +0100

    partially implemented the sensor fusion node (it receives a list of cone labels)

--- src/camera_package/camera_package/camera_node.py
+++ src/camera_package/camera_package/camera_node.py
@@ -14,7 +14,8 @@ class CameraNode(Node):
     def __init__(self):
         super().__init__('camera_node')
         self.timer_period = 0.  # seconds
-        self.cap = cv2.VideoCapture(0)
+        #self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.imread('/home/ubuntu/validation/images/12.png')
         self.bridge = CvBridge()
         self.publisher_ = self.create_publisher(Image, '/images/raw', 10)
         self.subscription_ = self.create_subscription(Float64, '/camera/freq', self.camera_ctrl_callback, 10)
@@ -23,7 +24,7 @@ class CameraNode(Node):
         self.timer.cancel() # to start with 0Hz
 
     def capture_image_callback(self):
-        ret, frame = self.cap.read()
+        #ret, frame = self.cap.read()
         frame = self.cap
         ret = True
         if ret:
--- src/camera_package/camera_package/image_processing_node.py
+++ src/camera_package/camera_package/image_processing_node.py
@@ -1,10 +1,13 @@
 import time
+from ctypes import Array
+
 import rclpy
 import cv2
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import torch
+from std_msgs.msg import Float32MultiArray
 
 
 class ImageProcessingNode(Node):
@@ -18,24 +21,35 @@ class ImageProcessingNode(Node):
         super().__init__('image_processing_node')
         self.bridge = CvBridge()
         self.publisher_ = self.create_publisher(Image, '/images/processed', 10)
+        self.publisher_labels = self.create_publisher(Float32MultiArray, '/images/labels', 10)
         self.subscription_ = self.create_subscription(Image, '/images/raw', self.process_image_callback, 10)
 
     def process_image_callback(self, data):
         start = time.time()
         self.get_logger().info('<- raw image')
-        frame = self.bridge.imgmsg_to_cv2(data)
 
+        frame = self.bridge.imgmsg_to_cv2(data)
 
         # downsize the image and convert to grayscale, to save bandwidth
         new_size = (640, 480)
         x_scale = new_size[0] / frame.shape[0]
         y_scale = new_size[1] / frame.shape[1]
         frame = cv2.resize(frame, dsize=new_size, fx=x_scale, fy=y_scale)
+
         cvtFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         output = self.model(cvtFrame)
         output.render()
+
+        npOut = output.pandas().xyxy[0].to_numpy()
+        npflat = npOut[:, :npOut.shape[1] - 1].flatten()
+
+        npmsg = Float32MultiArray()
+        for i in range(len(npflat)):
+            npmsg.data.append(npflat[i])
+        print(len(npmsg))
+        self.publisher_labels.publish(npmsg)
         frame = cv2.cvtColor(cvtFrame, cv2.COLOR_BGR2RGB)
-        print((time.time()- start)*1000)
+        print((time.time() - start) * 1000)
         self.publisher_.publish(self.bridge.cv2_to_imgmsg(frame))
         self.get_logger().info('processed image ->')
 
